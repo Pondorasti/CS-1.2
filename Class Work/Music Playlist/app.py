c2ebf0cc24f33f1e3f058fe45919dc2363e6d93f
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-10-30 23:11:33 -0700

    Music Playlist
    Finished Assignment Requirements

--- Class Work/Music Playlist/app.py
+++ Class Work/Music Playlist/app.py
@@ -8,6 +8,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
+
     def append(self, data):
         """Add a new node to the end of the list."""
         node = self.head
@@ -24,12 +25,45 @@ class LinkedList:
         """Add a new node to the start of the list."""
         self.head = self.Node(data, self.head)
     
+
     def print(self):
         """Print all the nodes in the list."""
         node = self.head
         while node is not None:
             print(node.data)
             node = node.next
+    
+    def delete_head(self):
+        """Removes the first node in the list"""
+        if self.head is not None:
+            self.head = self.head.next
+    
+    def delete_tail(self):
+        """Removes the last node in the list"""
+        node = self.head
+
+        if node is None:
+            return 
+        if node.next is None:
+            self.head = None
+            return
+        
+        while node.next.next is not None:
+            node = node.next
+        node.next = None
+
+    
+    def find(self, data):
+        """find() -> bool"""
+        node = self.head
+
+        while node is not None:
+            if node.data == data:
+                return True
+            
+            node = node.next
+        
+        return False
 
 
 
@@ -41,4 +75,14 @@ songs.prepend("Be happy")
 songs.append("Sam")
 songs.prepend("Rafa")
 
-songs.print()
+# songs.delete_head()
+# songs.delete_tail()
+# songs.delete_tail()
+
+# songs.delete_tail()
+# songs.delete_tail()
+# songs.delete_tail()
+
+print(songs.find("Be Happy"))
+
+# songs.print()
